be4ec54241dabc37122605454b6ffe35d3de9db6
Salvatore D'Avanzo <salvatore.davanzo89@gmail.com>
2019-12-12 17:27:24 +0100

    nuovo commit

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -8,32 +8,13 @@ class Bottles {
     public static void writeLyrics(PrintStream out) {
         for (int beers = 99; beers > -1; beers--) {
 
-
-            if (beers == 0) {
-                out.print(beers + actionBottle(beers) + " of beer on the wall,");
-                out.println(beers + actionBottle(beers) + " of beer,");
-                out.print(actionAfterBeer(beers));
-                out.println(beerLeft(beers) + actionBottle(beerLeft(beers)) + " of beer on the wall.\r\n");
-            } else if (beers == 1) {
-                out.print(beers + actionBottle(beers) + " of beer on the wall,");
-                out.println(beers + actionBottle(beers) + " of beer,");
-                out.print(actionAfterBeer(beers));
-                out.println(beerLeft(beers) + actionBottle(beerLeft(beers)) + " of beer on the wall.\r\n");
-            } else if (beers == 2) {
-                out.print(beers + actionBottle(beers) + " of beer on the wall,");
-                out.println(beers + actionBottle(beers) + " of beer,");
-                out.print(actionAfterBeer(beers));
-                out.println(beerLeft(beers) + actionBottle(beerLeft(beers)) + " of beer on the wall.\r\n");
-            } else {
-                out.print(beers + actionBottle(beers) + " of beer on the wall,");
-                out.println(beers + actionBottle(beers) + " of beer,");
-                out.print(actionAfterBeer(beers));
-                out.println(beerLeft(beers) + actionBottle(beerLeft(beers)) + " of beer on the wall.\r\n");
-            }
+            out.print(beers + actionBottle(beers) + " of beer on the wall,");
+            out.println(beers + actionBottle(beers) + " of beer,");
+            out.print(actionAfterBeer(beers));
+            out.println(beerLeft(beers) + actionBottle(beerLeft(beers)) + " of beer on the wall.\r\n");
         }
-
-
     }
+
 
     public static int beerLeft(int beers) {
         return (beers == 0) ? 99 : (beers - 1);
