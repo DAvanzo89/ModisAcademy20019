e85f66261687a7625f018c9d4af2dcaac3cbd625
Salvatore D'Avanzo <salvatore.davanzo89@gmail.com>
2019-12-12 15:45:46 +0100

    remove s variable

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -11,8 +11,8 @@ class Bottles {
 
 
             if (beers == 0) {
-                out.print(beers + " bottle" + s + " of beer on the wall,");
-                out.println(beers + " bottle" + s + " of beer,");
+                out.print(beers + " bottles"  + " of beer on the wall,");
+                out.println(beers + " bottles"  + " of beer,");
                 out.print("Go to the store, buy some more,");
                 out.println("99 bottles of beer on the wall.\r\n");
                 return;
