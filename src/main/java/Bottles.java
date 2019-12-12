564ad3da1af8fa5eac1c204c38cdd8511c4d85da
Salvatore D'Avanzo <salvatore.davanzo89@gmail.com>
2019-12-12 16:01:30 +0100

    delete variable s

--- src/main/java/Bottles.java
+++ src/main/java/Bottles.java
@@ -6,7 +6,6 @@ class Bottles {
     }
 
     public static void writeLyrics(PrintStream out) {
-        String s = "s";
         for (int beers = 99; beers > -1; beers--) {
 
 
@@ -28,13 +27,11 @@ class Bottles {
                     out.print(beers + " bottles" + " of beer on the wall,");
                     out.println(beers + " bottles"  + " of beer,");
                     out.print("Take one down, pass it around,");
-                    s = "";
                     out.println(beers - 1 + " bottle"  + " of beer on the wall.\r\n");
                 } else {
                     out.print(beers + " bottles" + " of beer on the wall,");
                     out.println(beers + " bottles"  + " of beer,");
                     out.print("Take one down, pass it around,");
-                    s = "s";
                     out.println(beers - 1 + " bottles" + " of beer on the wall.\r\n");
                 }
             }
